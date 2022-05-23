873abd79d33d86c1a4bfff9a674e39184db890dc
Robin Varetz <vrobin@student.42.fr>
2022-05-23 16:38:14 +0200

    add resources for offers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :offers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
