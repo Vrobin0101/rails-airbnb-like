637c9292a91bcd4c0846c9774a79386431d3052f
Maxime ANDRE <andremaxime7@gmail.com>
2022-05-23 15:07:48 +0200

    add validation to Offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,9 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :product_name, presence: true
+  validates :category, presence: true
+  validates :location, presence: true
+  validates :price, presence: true
 end
