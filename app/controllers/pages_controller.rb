1fca3dd92138c6c1525d6b66cf14e794d882f9a9
Marly Diallo <marly.marlou@gmail.com>
2022-05-24 18:30:52 +0200

    home cards and my profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :my_profile]
+
   def home
     @offers = Offer.select('DISTINCT category')
   end
