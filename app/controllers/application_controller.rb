390edc1dca7fcb21b961e20c9e37f9b33daf8e7e
Robin Varetz <vrobin@student.42.fr>
2022-05-24 12:40:06 +0200

    fix offers#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
 
-  after_action :verify_authorized, expect: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   private
