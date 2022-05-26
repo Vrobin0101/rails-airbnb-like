e2dba4639c7e7dc6c103c60652c93a1e66353a9c
Thibault Chassine <thibault.chassine@gmail.com>
2022-05-26 11:07:36 +0200

    added map with markers to offers index

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("flatpickr", FlatpickrController)
 
 import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
+
+import MapController from "./map_controller.js"
+application.register("map", MapController)
