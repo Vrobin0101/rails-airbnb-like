17385aec6128f68190cecad6b30abcf2a35aebac
Thibault Chassine <thibault.chassine@gmail.com>
2022-05-26 22:13:17 +0200

    add validation to offers for correct address and correct some other bugs

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,11 +3,24 @@ class Offer < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
-  validates :product_name, :category, :location, :price, :description, presence: true
+  validates :product_name, :category, :location, :price, :description, :photo, presence: true
   scope :filter_by_category, ->(category) { where category: category }
   scope :filter_by_location, ->(location) { where location: location }
   scope :filter_by_starts_with, ->(product_name) { where("product_name like ?", "#{product_name}%") }
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+  before_validation :lat_changed?
+
+  private
+
+  def lat_changed?
+    if (self.location_changed?)
+        if !(self.latitude_changed?)
+            self.errors.add(:location, "is not valid")
+            return false
+        end
+    end
+    return true
+  end
 end
