9bd1fc268d510148a9657223a676af5155e54eac
Thibault Chassine <thibault.chassine@gmail.com>
2022-05-26 15:40:00 +0200

    finish offer index with map and style

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/thibchss/cl3n09xy1007t15ld5hvm8vjf"
     })
 
     this.#addMarkersToMap()
